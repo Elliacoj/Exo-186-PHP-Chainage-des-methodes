bd0d4135e7e2b6f64ea6566c3b5dbd9b47a46126
Elliacoj <amaury.jocaille@hotmail.com>
2021-02-09 14:12:51 +0100

    Modif final

--- index.php
+++ index.php
@@ -12,3 +12,8 @@
  *
  * 5. Utilisez le chaînage des méthodes pour mettre à jour le nom et le prix en une seule ligne.
  */
+
+require "./classes/VODService.php";
+
+$vod = new VODService();
+$vod->setName("Bienvenu chez les chtis")->setPrice("9.99");
